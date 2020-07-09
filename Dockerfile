b61f1b36ff1cb3f49544dc79741f791e9c4e001f
carneirofc <claudiofcarneiro@hotmail.com>
2020-07-09 09:15:44 -0300

    Clean repo

--- Dockerfile
+++ Dockerfile
@@ -10,13 +10,6 @@ RUN apt-get -y update && apt-get -y install procps vim
 # Epics auto addr list
 ENV EPICS_CA_AUTO_ADDR_LIST YES
 
-# Base procServ port
-ENV EPICS_IOC_CAPUTLOG_INET 0.0.0.0
-ENV EPICS_IOC_CAPUTLOG_PORT 7012
-
-ENV EPICS_IOC_LOG_INET 0.0.0.0
-ENV EPICS_IOC_LOG_PORT 7011
-
 RUN mkdir -p /opt/cons-calc-ioc
 WORKDIR /opt/cons-calc-ioc
 
--- iocBoot/ioccalc/st.cmd
+++ iocBoot/ioccalc/st.cmd
@@ -4,24 +4,10 @@
 
 cd "${TOP}"
 
-## Register all support components
 dbLoadDatabase "dbd/calc.dbd"
 calc_registerRecordDeviceDriver pdbbase
 
-asSetFilename("${TOP}/db/Security.as")
-
-## Load record instances
 dbLoadRecords("${TOP}/db/Calc.db","P=Calc")
-
-#set_savefile_path("autosave")
-
-# Consts
-#set_pass0_restoreFile("${TOP}/autosave/Calc.sav")
-#set_pass1_restoreFile("${TOP}/autosave/Calc.sav")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-#caPutLogInit "10.128.255.4:7011" 2
-
-#create_monitor_set("${TOP}/autosave/Calc.req", 10)
